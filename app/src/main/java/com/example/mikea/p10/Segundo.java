02276f24d307927bb203df174b43516d34b260ee
Mike <mikeangellopez1@gmail.com>
2019-04-18 12:39:16 -0500

    Initial commit

---
+++ app/src/main/java/com/example/mikea/p10/Segundo.java
@@ -0,0 +1,13 @@
+package com.example.mikea.p10;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Segundo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_segundo);
+    }
+}
